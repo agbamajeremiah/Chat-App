87c59f3dd37792c708743b9b8f89a541ffa86eb4
Jerry Akem <agbamajeremiah@gmail.com>
2020-11-16 15:57:32 +0100

    Send invite message Feature

--- lib/ui/widgets/single_contact.dart
+++ lib/ui/widgets/single_contact.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+import 'package:MSG/services/authentication_service.dart';
+import 'package:MSG/locator.dart';
 import 'package:MSG/constant/route_names.dart';
 import 'package:MSG/models/chat.dart';
 import 'package:MSG/ui/shared/app_colors.dart';
 import 'package:MSG/ui/shared/ui_helpers.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SingleContact extends StatelessWidget {
   final String number;
@@ -24,7 +28,7 @@ class SingleContact extends StatelessWidget {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
       child: InkWell(
-        onTap: () {
+        onTap: () async {
           if (registered == true) {
             Navigator.pushNamed(
               context,
@@ -35,7 +39,24 @@ class SingleContact extends StatelessWidget {
               },
             );
           } else {
-            print("Contact not registered");
+            final AuthenticationSerivice _authenticationSerivice =
+                locator<AuthenticationSerivice>();
+            final _profileName = _authenticationSerivice.profileName;
+            if (number != null && _profileName != null) {
+              if (Platform.isAndroid) {
+                final _smsUrl =
+                    "sms: $number?body=$_profileName%20is%20inviting%20you%20to%20to%20join%20MSG.%20Download%20app%20https://www.google.com/";
+                if (await canLaunch(_smsUrl)) {
+                  await launch(_smsUrl);
+                }
+              } else if (Platform.isIOS) {
+                final _smsUrl =
+                    "sms: $number?body=$_profileName%20is%20inviting%20you%20to%20to%20join%20MSG.%20Download%20app%20https://www.google.com/";
+                if (await canLaunch(_smsUrl)) {
+                  await launch(_smsUrl);
+                }
+              }
+            }
           }
         },
         child: Row(
