6277c8c87f6dcdf3595ff52888f40ac6cf9ecd58
jeremiahagbama <agbamajeremiah@gmail.com>
2020-12-02 16:58:03 +0100

    Auth screen responsiveness

--- lib/viewmodels/register_viewModel.dart
+++ lib/viewmodels/register_viewModel.dart
@@ -15,9 +15,9 @@ class RegisterViewModel extends BaseViewModel {
     @required String phoneNumber,
   }) async {
     setBusy(true);
-    // var resposnse =
-    //     await _authenticationSerivice.register(phoneNumber: phoneNumber);
-    // print(resposnse);
+    var resposnse =
+        await _authenticationSerivice.register(phoneNumber: phoneNumber);
+    print(resposnse);
     _navigationService.navigateTo(OtpViewRoute, arguments: phoneNumber);
     setBusy(false);
   }
